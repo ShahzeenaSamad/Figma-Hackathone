83f2535ed4a1b1e85f7f362d7628e2e50d4f35b8
Shahzeena <shahzeenaashfaq92@gmail.com>
2024-12-09 21:50:06 +0500

    my figma

---
+++ src/app/components/Footer/page.tsx
@@ -0,0 +1,62 @@
+
+
+export default function Footer() {
+  return (
+      <footer className="relative">
+          <div className="w-[1441px] h-[589px] absolute left-[-1px] top-[3500px]">
+  <div className="w-[1441px] h-[499px] absolute left-[-1px] top-[90px]">
+    <div className="w-[1240px] h-[177px] absolute left-[100px] top-[140px]">
+      <div className="w-[1240px] h-[177px] relative">
+        <div className="w-[248px] h-[177px] absolute left-[0px] top-[0px]">
+        <div className="w-[248px] h-[114px] absolute left-[0px] top-[0px]">
+          <p className="font-extrabold text-lg"> SHOP.CO</p>
+          <p className="text-sm py-2"> We have clothes that suits your style and which you are proud to wear. From women to men.</p>
+        </div>
+
+        </div>
+        <div className="w-[104px] h-[177px] absolute left-[361.5px] top-[0px]">
+        <ul className="space-y-4">
+          <li className="font-semibold">COMPANY</li>
+          <li className="text-sm">about</li>
+          <li className="text-sm">Features</li>
+          <li className="text-sm">Works</li>
+          <li className="text-sm">Career</li>
+        </ul>
+        </div>
+        <div className="w-[136px] h-[177px] absolute left-[579px] top-[0px]">
+        <ul className="space-y-4">
+          <li className="font-semibold">HELP</li>
+          <li className="text-sm">Cutomer Support</li>
+          <li className="text-sm">Delivery Details</li>
+          <li className="text-sm">Terms & Conditions</li>
+          <li className="text-sm">Privacy Policy</li>
+        </ul>
+        </div>
+        <div className="w-[149px] h-[177px] absolute left-[828.5px] top-[0px]">
+        <ul className="space-y-4">
+          <li className="font-semibold">FAQ</li>
+          <li className="text-sm">Account</li>
+          <li className="text-sm">manage Deliveries</li>
+          <li className="text-sm">Orders</li>
+          <li className="text-sm">Payment</li>
+        </ul>
+        </div>
+        <div className="w-[149px] h-[177px] absolute left-[1091px] top-[0px]">
+        <ul className="space-y-4">
+          <li className="font-semibold">Resources</li>
+          <li className="text-sm">Free eBooks</li>
+          <li className="text-sm">Development Tutorial</li>
+          <li className="text-sm">How to-Blog</li>
+          <li className="text-sm">Youtube Playlist</li>
+        </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+      </footer>
+  );
+}
